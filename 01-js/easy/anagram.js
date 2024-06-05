e4609ddbaf940e1bd632262a6c04fd2613152d62
Dhananjay Maurya <dhananjay.99jb@gmail.com>
2024-06-05 23:18:48 +0530

    chore: Update npm dependency to latest stable version and add Jest as a new dependency

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -5,7 +5,16 @@
 */
 
 function isAnagram(str1, str2) {
+  // Convert the strings to lowercase and remove any non-alphabetic characters
+  const cleanStr1 = str1.toLowerCase().replace(/[^a-z]/g, '');
+  const cleanStr2 = str2.toLowerCase().replace(/[^a-z]/g, '');
 
+  // Sort the characters in the strings
+  const sortedStr1 = cleanStr1.split('').sort().join('');
+  const sortedStr2 = cleanStr2.split('').sort().join('');
+
+  // Compare the sorted strings
+  return sortedStr1 === sortedStr2;
 }
 
 module.exports = isAnagram;
